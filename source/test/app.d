258354b42449349c3c178f300c9d79c5ca8d28db
Colden Cullen <colden@coldencullen.com>
2014-10-05 14:49:26 -0400

    Progress on having v8 not crash, added platform specific configs and bins

--- source/test/app.d
+++ source/test/app.d
@@ -5,46 +5,39 @@ import std.stdio, std.typecons;
 
 unittest
 {
-    V8.Initialize();
-    V8.Dispose();
+    write( "Starting... Press enter to continue..." );
+    readln();
 
     V8.InitializeICU();
-    Platform platform = CreateDefaultPlatform();
-    V8.InitializePlatform( platform );
+    //Platform platform = CreateDefaultPlatform();
+    //V8.InitializePlatform( p );
     V8.Initialize();
-
-    writeln( "Initialized!" );
-
-    auto desc = Isolate.CreateParams();
-    Isolate isolate = Isolate.New( desc );
-    {
-        auto isolate_scope = scoped!( Isolate.Scope )( isolate );
-
-        writeln( "Scoped!" );
-    }
 
     //V8.SetFlagsFromCommandLine(&argc, argv, true);
     //ShellArrayBufferAllocator array_buffer_allocator;
     //V8.SetArrayBufferAllocator(&array_buffer_allocator);
-    /*Isolate* isolate = Isolate.New();
-    run_shell = (argc == 1);
-    int result;
+
+    writeln( "Initialized!" );
+
+    Isolate isolate = Isolate.New();
     {
-        v8::Isolate::Scope isolate_scope(isolate);
-        v8::HandleScope handle_scope(isolate);
-        v8::Handle<v8::Context> context = CreateShellContext(isolate);
+        auto isolate_scope = scoped!( Isolate.Scope )( isolate );
+        //auto handle_scope = scoped!( HandleScope )( isolate );
+
+        /*
+        Handle<Context> context = CreateShellContext(isolate);
         if (context.IsEmpty()) {
-          fprintf(stderr, "Error creating context\n");
-          return 1;
+            fprintf(stderr, "Error creating context\n");
+            return 1;
         }
-        v8::Context::Scope context_scope(context);
-        result = RunMain(isolate, argc, argv);
-        if (run_shell) RunShell(context);
-    }*/
+        auto context_scope = scoped!( Context.Scope )( context );
+        */
+
+        writeln( "Scoped!" );
+    }
     V8.Dispose();
     V8.ShutdownPlatform();
     //delete platform;
-    //return result;
 
     writeln( "Shutdown!" );
 }
--- source/v8/v8.d
+++ source/v8/v8.d
@@ -1,5 +1,7 @@
 module v8.v8;
 
+import core.stdc.stdint;
+
 extern(C++, v8) // namespace v8
 {
     class V8
@@ -44,6 +46,59 @@ extern(C++, v8) // namespace v8
         bool Dispose();
     }
 
+    //pragma( mangle, class )
+    struct ResourceConstraints
+    {
+    public:
+        //TODO
+        //this();
+
+        /**
+         * Configures the constraints with reasonable default values based on the
+         * capabilities of the current device the VM is running on.
+         *
+         * \param physical_memory The total amount of physical memory on the current
+         *   device, in bytes.
+         * \param virtual_memory_limit The amount of virtual memory on the current
+         *   device, in bytes, or zero, if there is no limit.
+         * \param number_of_processors The number of CPUs available on the current
+         *   device.
+         */
+        //TODO
+        /*
+        void ConfigureDefaults(uint64_t physical_memory,
+                               uint64_t virtual_memory_limit,
+                               uint32_t number_of_processors);
+        */
+
+        int max_semi_space_size() const { return max_semi_space_size_; }
+        void set_max_semi_space_size(int value) { max_semi_space_size_ = value; }
+        int max_old_space_size() const { return max_old_space_size_; }
+        void set_max_old_space_size(int value) { max_old_space_size_ = value; }
+        int max_executable_size() const { return max_executable_size_; }
+        void set_max_executable_size(int value) { max_executable_size_ = value; }
+        uint32_t* stack_limit() /*const*/ { return stack_limit_; }
+        // Sets an address beyond which the VM's stack may not grow.
+        void set_stack_limit(uint32_t* value) { stack_limit_ = value; }
+        int max_available_threads() const { return max_available_threads_; }
+        // Set the number of threads available to V8, assuming at least 1.
+        void set_max_available_threads(int value) {
+            max_available_threads_ = value;
+        }
+        size_t code_range_size() const { return code_range_size_; }
+        void set_code_range_size(size_t value) {
+            code_range_size_ = value;
+        }
+
+    private:
+        int max_semi_space_size_;
+        int max_old_space_size_;
+        int max_executable_size_;
+        uint32_t* stack_limit_;
+        int max_available_threads_;
+        size_t code_range_size_;
+    }
+
     class Isolate
     {
         /**
@@ -52,31 +107,30 @@ extern(C++, v8) // namespace v8
         struct CreateParams
         {
             /**
-            * The optional entry_hook allows the host application to provide the
-            * address of a function that's invoked on entry to every V8-generated
-            * function.  Note that entry_hook is invoked at the very start of each
-            * generated function. Furthermore, if an  entry_hook is given, V8 will
-            * always run without a context snapshot.
-            */
+             * The optional entry_hook allows the host application to provide the
+             * address of a function that's invoked on entry to every V8-generated
+             * function.  Note that entry_hook is invoked at the very start of each
+             * generated function. Furthermore, if an  entry_hook is given, V8 will
+             * always run without a context snapshot.
+             */
             //TODO
             //FunctionEntryHook entry_hook;
 
             /**
-            * Allows the host application to provide the address of a function that is
-            * notified each time code is added, moved or removed.
-            */
+             * Allows the host application to provide the address of a function that is
+             * notified each time code is added, moved or removed.
+             */
             //TODO
             //JitCodeEventHandler code_event_handler;
 
             /**
-            * ResourceConstraints to use for the new Isolate.
-            */
-            //TODO
-            //ResourceConstraints constraints;
+             * ResourceConstraints to use for the new Isolate.
+             */
+            ResourceConstraints constraints;
 
             /**
-            * This flag currently renders the Isolate unusable.
-            */
+             * This flag currently renders the Isolate unusable.
+             */
             bool enable_serializer;
         }
 
@@ -112,6 +166,12 @@ extern(C++, v8) // namespace v8
          *
          * V8::Initialize() must have run prior to this.
          */
+        static Isolate New()
+        {
+            CreateParams params;
+            return New( params );
+        }
+        /// ditto
         static Isolate New( const ref CreateParams params );
 
         /**
@@ -151,6 +211,31 @@ extern(C++, v8) // namespace v8
 
     }
 
+    /**
+     * A stack-allocated class that governs a number of local handles.
+     * After a handle scope has been created, all local handles will be
+     * allocated within that handle scope until either the handle scope is
+     * deleted or another handle scope is created.  If there is already a
+     * handle scope and a new one is created, all allocations will take
+     * place in the new handle scope until it is deleted.  After that,
+     * new handles will again be allocated in the original handle scope.
+     *
+     * After the handle scope of a local handle has been deleted the
+     * garbage collector will no longer track the object stored in the
+     * handle and may deallocate it.  The behavior of accessing a handle
+     * for which the handle scope has been deleted is undefined.
+     */
+    class HandleScope
+    {
+    public:
+        //TODO
+        //this( Isolate isolate );
+
+        static int NumberOfHandles( Isolate isolate );
+        //TODO
+        //Isolate GetIsolate() const;
+    }
+
     class Value
     {
 
@@ -163,6 +248,7 @@ extern(C++, v8) // namespace v8
 
     extern( C++, platform )
     {
-        Platform CreateDefaultPlatform( int thread_pool_size = 0 );
+        //TODO
+        //Platform CreateDefaultPlatform( int thread_pool_size = 0 );
     }
 }
